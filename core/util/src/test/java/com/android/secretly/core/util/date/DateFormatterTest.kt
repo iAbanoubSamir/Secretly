55b04c35d56fd6eff0605ec091ca3eae3232c860
Abanoub Samir <abanoub.sameer.22@gmail.com>
2024-06-28 19:54:14 +0300

    Refactor format date and separate class

--- core/util/src/test/java/com/android/secretly/core/util/date/DateFormatterTest.kt
+++ core/util/src/test/java/com/android/secretly/core/util/date/DateFormatterTest.kt
@@ -2,22 +2,16 @@ package com.android.secretly.core.util.date
 
 import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 
 class DateFormatterTest {
 
     @Test
     fun `formatDate, returns formatted date string`() {
+        val dateFormatter = DateFormatter()
 
-        val dateFormatter = formatDate(1719592620115)
+        val result = dateFormatter.format(1719592620115)
 
-        assertThat(dateFormatter).isEqualTo("28-06-2024")
+        assertThat(result).isEqualTo("28-06-2024")
     }
 
-    private fun formatDate(date: Long): String {
-        val dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH)
-        return dateFormat.format(Date(date))
-    }
 }
