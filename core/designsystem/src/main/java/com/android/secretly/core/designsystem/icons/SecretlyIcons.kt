825e6e185243624c6688b22e9dc6aa1a28f3e8fa
Abanoub Samir <abanoub.sameer.22@gmail.com>
2024-06-28 18:13:20 +0300

    Setup initial material icons set

---
+++ core/designsystem/src/main/java/com/android/secretly/core/designsystem/icons/SecretlyIcons.kt
@@ -0,0 +1,49 @@
+package com.android.secretly.core.designsystem.icons
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.automirrored.rounded.Send
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.LocationOn
+import androidx.compose.material.icons.rounded.Lock
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.Phone
+import androidx.compose.material.icons.rounded.Refresh
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material.icons.rounded.Share
+import androidx.compose.material.icons.rounded.Warning
+
+/**
+ * Secretly icons.
+ */
+object SecretlyIcons {
+    val Home = Icons.Rounded.Home
+    val HomeBorder = Icons.Outlined.Home
+    val Person = Icons.Rounded.Person
+    val PersonBorder = Icons.Outlined.Person
+    val Settings = Icons.Rounded.Settings
+    val SettingsBorder = Icons.Outlined.Settings
+    val Favourite = Icons.Rounded.Favorite
+    val FavouriteBorder = Icons.Outlined.FavoriteBorder
+    val Search = Icons.Rounded.Search
+    val Close = Icons.Rounded.Close
+    val Refresh = Icons.Rounded.Refresh
+    val Notifications = Icons.Rounded.Notifications
+    val Share = Icons.Rounded.Share
+    val Email = Icons.Rounded.Email
+    val LocationOn = Icons.Rounded.LocationOn
+    val Lock = Icons.Rounded.Lock
+    val Phone = Icons.Rounded.Phone
+    val Warning = Icons.Rounded.Warning
+    val Send = Icons.AutoMirrored.Rounded.Send
+    val ArrowBack = Icons.AutoMirrored.Rounded.ArrowBack
+}
